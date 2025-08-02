8dfafa31483121dcc1dbff5d61ec9428eb7a65aa
ProteinK <konprotein@gmail.com>
2025-08-02 18:17:39 +0200

    Add content to pages

--- src/about.js
+++ src/about.js
@@ -4,6 +4,19 @@ const buildAbout = () => {
   const result = document.createElement("div");
   result.classList.add("about");
 
+  const h1 = document.createElement('h1');
+  h1.textContent = "About";
+  h1.classList.add("about");
+  result.appendChild(h1);
+
+  const authorP = document.createElement("p");
+  authorP.innerHTML = 'Created by <a href="https://github.com/ProteinK">ProteinK</a>';
+  result.appendChild(authorP);
+
+  const creditsP = document.createElement("p");
+  creditsP.innerHTML = 'Photo by <a href="https://unsplash.com/@alanaktion?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Alan Hardman</a> on <a href="https://unsplash.com/photos/pepperoni-pizza-SU1LFoeEUkk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>';
+  result.appendChild(creditsP);
+
   return result;
 };
 
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,33 @@
+const getRandomInt = (min, max) => {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
 const buildMenu = () => {
   const result = document.createElement("div");
   result.classList.add("menu");
+
+  const h1 = document.createElement('h1');
+  h1.textContent = "Menu";
+  h1.classList.add("menu");
+  result.appendChild(h1);
+
+  const menu = document.createElement('div');
+  menu.classList.add("menuItems");
+
+  for (let i = 0; i < 5; i++) {
+    const item = document.createElement("p");
+    item.textContent = `Randomly Generated Pizza ${i + 1}`;
+    const itemPrice = document.createElement("p");
+    const randomPrice = getRandomInt(15, 31);
+    itemPrice.textContent = `$${randomPrice}`;
+
+    menu.appendChild(item);
+    menu.appendChild(itemPrice);
+  }
+
+  result.appendChild(menu);
 
   return result;
 };
